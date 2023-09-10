b7faedf17e7a81e08606762046a70a55452d4011
Andrey <gaponow@yandex.ru>
2023-09-10 22:23:39 +0300

    Remove / in route application

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import store from "./store/index";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-// axios.defaults.baseURL = 'http://127.0.0.1:8000/';  // the FastAPI backend
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';  // the FastAPI backend
 // axios.defaults.baseURL = 'https://visa-bro.ru/';  // the FastAPI backend
 // axios.defaults.baseURL = 'http://212.22.94.99/api';  // the test FastAPI backend
-axios.defaults.baseURL = '/api';  // the test FastAPI backend
+// axios.defaults.baseURL = '/api';  // the test FastAPI backend
 
 const app = createApp(App)
 // components.forEach(component=>{
